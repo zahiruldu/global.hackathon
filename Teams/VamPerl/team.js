e16b289c0a55797b9817137c85690e068707c8ec
Köble István <kobleistvan@gmail.com>
2014-11-26 21:42:58 +0200

    Added initial team info

---
+++ Teams/VamPerl/team.js
@@ -0,0 +1,22 @@
+{
+  "teamName": "PerlMonks",
+  "members": [
+    {
+      "name": "Bărbânță Ligia Luoana",
+      "koding": "luoana",
+      "location": "Cluj-Napoca, RO"
+    },
+    {
+      "name": "Köble István",
+      "twitter": "kobleistvan",
+      "koding": "kobleistvan",
+      "location": "Cluj-Napoca, RO"
+    },
+    {
+      "name": "Alexandru Străjeriu",
+      "twitter": "deluxaran",
+      "koding": "deluxaran",
+      "location": "Cluj-Napoca, RO"
+    }
+  ]
+}
