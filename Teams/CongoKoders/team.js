49e3a98e1e6a19f98bb8ce4806cf03da2368f3dc
bwalya <hervebwalya@gmail.com>
2014-11-27 12:29:00 +0200

    CongoKoders team

---
+++ Teams/CongoKoders/team.js
@@ -0,0 +1,17 @@
+{
+  "teamName": "CongoKoders",
+  "members": [
+    {
+      "name": "Herve Bupe Bwalya",
+      "twitter": "HerveBupe",
+      "koding": "hervebwalya",
+      "location": "Lubumbashi, CD"
+    },
+    {
+      "name": "Carl Kitenge Kasongo",
+      "twitter": "CarlKHimSelf",
+      "koding": "carl.kitenge",
+      "location": "Lubumbashi, CD"
+    }
+  ]
+}
