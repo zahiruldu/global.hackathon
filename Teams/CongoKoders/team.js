5bcc9a9e00faa3935cac63105e3c1086095ff130
bwalya <hervebwalya@gmail.com>
2014-11-28 09:57:39 +0200

    CongoKoders team updated

--- Teams/CongoKoders/team.js
+++ Teams/CongoKoders/team.js
@@ -6,12 +6,6 @@
       "twitter": "HerveBupe",
       "koding": "hervebwalya",
       "location": "Lubumbashi, CD"
-    },
-    {
-      "name": "Carl Kitenge Kasongo",
-      "twitter": "CarlKHimSelf",
-      "koding": "carl.kitenge",
-      "location": "Lubumbashi, CD"
     }
   ]
 }
