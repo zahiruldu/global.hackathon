de7de8b7b3b16f4e5c09b147bb43f56bc92e3762
silbo <silver.kuusik@gmail.com>
2014-12-02 13:38:59 +0100

    Create team.js

---
+++ Teams/ninjasinthebox/team.js
@@ -0,0 +1,24 @@
+{
+  "teamName": "hobosinthebox",
+  "members": [
+    {
+      "name": "silbo",
+      "twitter": "",
+      "koding": "silbo",
+      "location": "Kiel, DE",
+      "lead": true
+    },
+    {
+      "name": "dono",
+      "twitter": "",
+      "koding": "",
+      "location": "Kiel, DE"
+    },
+    {
+      "name": "morris",
+      "twitter": "",
+      "koding": "morriswinkler",
+      "location": "Berlin, DE"
+    }
+  ]
+}
